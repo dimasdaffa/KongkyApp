cb489140ffc99e0b97944ca21e5f1c8aecb5bd1d
dimasdaffa <dimas.de2004@gmail.com>
2026-03-14 21:39:23 +0700

    feat: Add CalendarView, MainTabView, and ProfileView components with initial implementation

---
+++ KongkyApp/Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  KongkyApp
+//
+//  Created by DIMAS DAFFA ERNANDA on 14/03/26.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
---
+++ KongkyApp/Views/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  KongkyApp
+//
+//  Created by DIMAS DAFFA ERNANDA on 14/03/26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
