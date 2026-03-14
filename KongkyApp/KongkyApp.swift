b49949ae1b09784fde88b17cc293fca89d3ecb08
dimasdaffa <dimas.de2004@gmail.com>
2026-03-14 12:39:43 +0700

    Initial Commit

---
+++ KongkyApp/KongkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KongkyAppApp.swift
+//  KongkyApp
+//
+//  Created by DIMAS DAFFA ERNANDA on 14/03/26.
+//
+
+import SwiftUI
+
+@main
+struct KongkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
