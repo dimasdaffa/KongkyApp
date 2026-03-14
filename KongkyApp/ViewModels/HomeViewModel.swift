6fe2fc939a22f7cf810f244fb338e730b156a1fb
dimasdaffa <dimas.de2004@gmail.com>
2026-03-14 21:28:44 +0700

    feat: Add initial model and view files for Event and HomeView components

---
+++ KongkyApp/ViewModels/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  KongkyApp
+//
+//  Created by DIMAS DAFFA ERNANDA on 14/03/26.
+//
+
+import Foundation
---
+++ KongkyApp/Views/Detail/ActivityDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityDetailView.swift
+//  KongkyApp
+//
+//  Created by DIMAS DAFFA ERNANDA on 14/03/26.
+//
+
+import SwiftUI
+
+struct ActivityDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityDetailView()
+}
