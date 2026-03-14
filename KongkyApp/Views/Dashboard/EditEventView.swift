3b2b7a2e22dfd982d7d7bb943a1572a6c84783ac
dimasdaffa <dimas.de2004@gmail.com>
2026-03-15 00:51:59 +0700

    feat: Add EditEventView with initial UI setup for editing events

---
+++ KongkyApp/Views/Dashboard/EditEventView.swift
@@ -0,0 +1,18 @@
+//
+//  EditEventView.swift
+//  KongkyApp
+//
+//  Created by DIMAS DAFFA ERNANDA on 15/03/26.
+//
+
+import SwiftUI
+
+struct EditEventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditEventView()
+}
