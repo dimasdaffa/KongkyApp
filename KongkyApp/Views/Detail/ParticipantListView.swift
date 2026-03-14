5fb2b4f1f8fc3251c21c61b681ea953b0bbc647d
dimasdaffa <dimas.de2004@gmail.com>
2026-03-14 23:23:11 +0700

    feat: Add initial implementation of ParticipantListView with placeholder text

---
+++ KongkyApp/Views/Detail/ParticipantListView.swift
@@ -0,0 +1,18 @@
+//
+//  ParticipantListView.swift
+//  KongkyApp
+//
+//  Created by DIMAS DAFFA ERNANDA on 14/03/26.
+//
+
+import SwiftUI
+
+struct ParticipantListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParticipantListView()
+}
