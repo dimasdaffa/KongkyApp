6ed70ffcf6de7dfcf194d5e2a198ce8f50fd17ee
dimasdaffa <dimas.de2004@gmail.com>
2026-03-15 00:17:18 +0700

    feat: Add CreateEventView and MyActivitiesView with initial UI setup

---
+++ KongkyApp/Views/Profile/MyActivitiesView.swift
@@ -0,0 +1,18 @@
+//
+//  MyActivitiesView.swift
+//  KongkyApp
+//
+//  Created by DIMAS DAFFA ERNANDA on 15/03/26.
+//
+
+import SwiftUI
+
+struct MyActivitiesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyActivitiesView()
+}
