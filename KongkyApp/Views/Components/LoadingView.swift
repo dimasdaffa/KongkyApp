80afd65ab0cf407ada555cd2b51f9174802a56da
dimasdaffa <dimas.de2004@gmail.com>
2026-03-15 10:02:15 +0700

    feat: Add loading animation

---
+++ KongkyApp/Views/Components/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  KongkyApp
+//
+//  Created by DIMAS DAFFA ERNANDA on 15/03/26.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
