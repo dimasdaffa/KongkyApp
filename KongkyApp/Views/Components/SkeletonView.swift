264f0d0b1d1126297720517c512914230b1a1298
dimasdaffa <dimas.de2004@gmail.com>
2026-03-15 10:40:52 +0700

    feat: Implement skeleton loading states for Dashboard and Event views using new skeleton components.

---
+++ KongkyApp/Views/Components/SkeletonView.swift
@@ -0,0 +1,18 @@
+//
+//  SkeletonView.swift
+//  KongkyApp
+//
+//  Created by DIMAS DAFFA ERNANDA on 15/03/26.
+//
+
+import SwiftUI
+
+struct SkeletonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkeletonView()
+}
