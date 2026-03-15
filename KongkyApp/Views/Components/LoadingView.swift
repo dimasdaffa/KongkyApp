6617622d611457653d97ceb9268330180420d3f8
dimasdaffa <dimas.de2004@gmail.com>
2026-03-15 10:02:32 +0700

    feat: Revise loading animation

--- KongkyApp/Views/Components/LoadingView.swift
+++ KongkyApp/Views/Components/LoadingView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct LoadingView: View {
+    @State private var rotation: Double = 0
+    
+    var message: String = "Loading..."
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.white.ignoresSafeArea()
+            
+            VStack(spacing: 24) {
+                // THE SPINNING LOGO
+                ZStack {
+                    // Background track circle
+                    Circle()
+                        .stroke(Color.gray.opacity(0.2), lineWidth: 6)
+                        .frame(width: 80, height: 80)
+                    
+                    // The spinning black segment
+                    Circle()
+                        .trim(from: 0, to: 0.7) // Only draws 70% of the circle
+                        .stroke(Color.black, style: StrokeStyle(lineWidth: 6, lineCap: .round))
+                        .frame(width: 80, height: 80)
+                        .rotationEffect(.degrees(rotation))
+                        .onAppear {
+                            withAnimation(Animation.linear(duration: 1).repeatForever(autoreverses: false)) {
+                                rotation = 360
+                            }
+                        }
+                    
+                    // Our App Logo in the middle
+                    Text("K")
+                        .font(.system(size: 32, weight: .bold))
+                }
+                
+                Text(message)
+                    .font(.headline)
+                    .foregroundColor(.gray)
+                    .tracking(2)
+            }
+        }
     }
 }
 
 #Preview {
-    LoadingView()
+    LoadingView(message: "Warming things up...")
 }
